f783f6ba943bff599cdc1d6b2aebc2237f0fecca
RIAN-PC\Rian <RSchmit@student.neumont.edu>
2019-06-05 00:06:35 -0600

    A work in progress sheep collecting minigame

--- Assets/Scripts/Sheep.cs
+++ Assets/Scripts/Sheep.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Sheep : MonoBehaviour
 {
-    public Vector2 startPosition = Vector2.zero;
     public Vector2 endPosition = Vector2.zero;
-    public float speed = 0.0f;
+    [Range(1.0f, 10.0f)]
+    public float speed = 1.0f;
 
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, endPosition, Time.deltaTime * speed);
     }
 
+    float angle = 0.0f;
     IEnumerator Hop()
     {
-        yield return null;
+        while(true)
+        {
+            angle += Time.deltaTime;
+            angle %= 360.0f;
+
+            yield return null;
+        }
     }
 
     public void Collect()
@@ -28,4 +34,6 @@ public class Sheep : MonoBehaviour
         StopAllCoroutines();
         Destroy(this.gameObject);
     }
+
+
 }
---
+++ Assets/Scripts/SheepMinigame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepMinigame : MonoBehaviour
+{
+
+    [SerializeField]
+    BoxCollider2D spawnArea;
+
+    [SerializeField]
+    Sheep sheepPrefab;
+
+    void Start()
+    {
+        Sheep sheep = GameObject.Instantiate(sheepPrefab, RandomPointInBounds2D(spawnArea.bounds), Quaternion.identity);
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    Vector2 RandomPointInBounds2D(Bounds bounds)
+    {
+        return new Vector2(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y));
+    }
+}
