c4151a49335348bb55e6694ecb2f40ecfe2eeb54
RSchmit <RSchmit@student.neumont.edu>
2019-06-04 19:47:07 -0600

    The start of the sheep collecting minigame

---
+++ Assets/Scripts/Sheep.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+    public Vector2 startPosition = Vector2.zero;
+    public Vector2 endPosition = Vector2.zero;
+    public float speed = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Hop()
+    {
+        yield return null;
+    }
+
+    public void Collect()
+    {
+        StopAllCoroutines();
+        Destroy(this.gameObject);
+    }
+}
