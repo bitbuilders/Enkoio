e69408cd71aaf70f7ebf187222abcedfd3332f13
RSchmit <RSchmit@student.neumont.edu>
2019-06-06 17:28:06 -0600

    Added cleanup.

--- Assets/Scripts/SheepMinigame.cs
+++ Assets/Scripts/SheepMinigame.cs
@@ -27,6 +27,8 @@ public class SheepMinigame : Singleton<SheepMinigame>
 
     bool inGame = false;
 
+    List<GameObject> sheep = null;
+
     void Start()
     {
         StartGame();
@@ -74,6 +76,7 @@ public class SheepMinigame : Singleton<SheepMinigame>
                     s.transform.position = RandomPointInBounds2D(spawnArea.bounds);
                     s.endPosition = RandomPointInBounds2D(endArea.bounds);
                     s.speed = Random.Range(2.0f, 4.0f);
+                    sheep.Add(s.gameObject);
                 }
                 yield return new WaitForSeconds(1.0f);
             }
@@ -86,6 +89,7 @@ public class SheepMinigame : Singleton<SheepMinigame>
 
     public void StartGame()
     {
+        sheep = new List<GameObject>();
         timeLeft = minigameTime;
         numCollected = 0;
         numSheep = 0;
@@ -98,6 +102,13 @@ public class SheepMinigame : Singleton<SheepMinigame>
     {
         inGame = false;
         StopAllCoroutines();
+        foreach(GameObject GO in sheep)
+        {
+            if(GO != null)
+            {
+                Destroy(GO);
+            }
+        }
     }
 
     Vector2 RandomPointInBounds2D(Bounds bounds)
